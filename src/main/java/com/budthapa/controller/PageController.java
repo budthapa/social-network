9d9d5f7d77d046e6d25150bb77c5d27bea8490cd
Buddhi Bal Thapa <budthapa@gmail.com>
2017-02-11 10:07:17 +0400

    First draft of the project

---
+++ src/main/java/com/budthapa/controller/PageController.java
@@ -0,0 +1,20 @@
+package com.budthapa.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class PageController {
+	static final Logger log = LoggerFactory.getLogger(PageController.class);
+	
+	@RequestMapping(method=RequestMethod.GET, value="/")
+	String index(){
+		log.info("Serving index page");
+		return "index";
+	}
+}
