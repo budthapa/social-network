19a9b4c73099fcdf6d8184391d992347f84f9a89
Buddhi Bal Thapa <budthapa@gmail.com>
2017-02-11 23:17:17 +0400

    created domain status

---
+++ src/main/java/com/budthapa/domain/StatusUpdate.java
@@ -0,0 +1,65 @@
+package com.budthapa.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="status_update")
+public class StatusUpdate {
+	@Id
+	@Column(name="status_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name="status")
+	private String text;
+	
+	@Column(name="added_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date added;
+	
+	@PrePersist
+	private void onCreate(){
+		if(added==null){
+			added=new Date();
+		}
+	}
+
+	public StatusUpdate(String text){
+		this.text=text;
+	}
+	
+	public StatusUpdate(String text, Date added){
+		this.text=text;
+		this.added=added;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Date getAdded() {
+		return added;
+	}
+	public void setAdded(Date added) {
+		this.added = added;
+	}
+	
+}
