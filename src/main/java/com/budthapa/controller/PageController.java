98e35a78f1a0d4d706f042a451a6429520544b8c
Buddhi Bal Thapa <budthapa@gmail.com>
2017-02-12 18:58:50 +0400

    added validation on form, added properties file for validation

--- src/main/java/com/budthapa/controller/PageController.java
+++ src/main/java/com/budthapa/controller/PageController.java
@@ -1,10 +1,14 @@
 package com.budthapa.controller;
 
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,29 +25,26 @@ public class PageController {
 	
 	@RequestMapping(method=RequestMethod.GET, value="/")
 	String index(){
-		log.info("Serving index page");
 		return "index";
 	}
 	
 	@RequestMapping(method=RequestMethod.GET, value={"/addstatus", "/addStatus"})
-	ModelAndView addStatus(ModelAndView modelAndView){
-		log.info("Serving status page");
+	ModelAndView addStatus(ModelAndView modelAndView, @ModelAttribute("statusUpdate") StatusUpdate statusUpdate){
 		modelAndView.setViewName("addStatus");
-		StatusUpdate statusUpdate=new StatusUpdate();
-		//StatusUpdate latestStatusUpdate=statusUpdateService.getLatest();
-		
-		modelAndView.getModel().put("statusUpdate", statusUpdate);
-		modelAndView.getModel().put("latestStatusUpdate", latestStatusUpdate());
-		
+		modelAndView.getModel().put("latestStatusUpdate", latestStatusUpdate());		
 		return modelAndView;
 	}
 	
 	@RequestMapping(value="/addStatus",method=RequestMethod.POST)
-	ModelAndView addStatus(ModelAndView modelAndView, StatusUpdate statusUpdate){
-		log.info("Saving status");
+	ModelAndView addStatus(ModelAndView modelAndView, @ModelAttribute("statusUpdate") @Valid StatusUpdate statusUpdate, 
+			BindingResult result){
 		modelAndView.setViewName("addStatus");
-		statusUpdateService.save(statusUpdate);
-		//StatusUpdate latestStatusUpdate=statusUpdateService.getLatest();
+		
+		if(!result.hasErrors()){
+			statusUpdateService.save(statusUpdate);			
+			modelAndView.getModel().put("statusUpdate", new StatusUpdate());
+		}
+		
 		modelAndView.getModel().put("latestStatusUpdate", latestStatusUpdate());
 		return modelAndView;
 	}
