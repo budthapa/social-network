9468a04616caf96eac198ae498450a20392db871
Buddhi Bal Thapa <budthapa@gmail.com>
2017-02-21 14:35:20 +0400

    apply inmemory authentication

--- src/main/java/com/budthapa/security/WebSecurityConfig.java
+++ src/main/java/com/budthapa/security/WebSecurityConfig.java
@@ -3,7 +3,9 @@
  */
 package com.budthapa.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,5 +31,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.defaultSuccessUrl("/")
 				.permitAll();
 	}
+	/*
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		auth.inMemoryAuthentication().withUser("buddhi").password("123456").roles("USER");
+	}
+	*/
+	@Autowired
+	 public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {    
+	  auth.inMemoryAuthentication().withUser("buddhi").password("123456").roles("USER");
+	 }
+	
 
 }
