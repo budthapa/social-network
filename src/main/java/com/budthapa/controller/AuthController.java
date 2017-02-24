4c37beb805edcddd4368e4724b6d49761d438775
Buddhi Bal Thapa <budthapa@gmail.com>
2017-02-24 08:36:39 +0400

    created a new user registration form

--- src/main/java/com/budthapa/controller/AuthController.java
+++ src/main/java/com/budthapa/controller/AuthController.java
@@ -2,12 +2,27 @@ package com.budthapa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.budthapa.domain.SiteUser;
 
 @Controller
 public class AuthController {
+	
+	
 	@RequestMapping("/login")
 	String admin(){
 		return "login";
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.GET )
+	ModelAndView register(ModelAndView modelAndView){
+
+		SiteUser user=new SiteUser();
+		modelAndView.getModel().put("user", user);
+		modelAndView.setViewName("register");
+		return modelAndView;
 	}
 	
 }
