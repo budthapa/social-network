e260ad0c85be0c27d2f23e8754093c1b7cd0508f
Buddhi Bal Thapa <budthapa@gmail.com>
2017-02-24 09:18:22 +0400

    register new user

--- src/main/java/com/budthapa/controller/AuthController.java
+++ src/main/java/com/budthapa/controller/AuthController.java
@@ -1,15 +1,22 @@
 package com.budthapa.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.budthapa.domain.SiteUser;
+import com.budthapa.service.UserService;
 
 @Controller
 public class AuthController {
 	
+	@Autowired
+	UserService userService;
 	
 	@RequestMapping("/login")
 	String admin(){
@@ -22,6 +29,16 @@ public class AuthController {
 		SiteUser user=new SiteUser();
 		modelAndView.getModel().put("user", user);
 		modelAndView.setViewName("register");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	ModelAndView register(ModelAndView modelAndView, @Valid SiteUser user, BindingResult result){
+		modelAndView.setViewName("register");
+		if(!result.hasErrors()){
+			userService.register(user);
+			modelAndView.setViewName("redirect:/");
+		}
 		return modelAndView;
 	}
 	
